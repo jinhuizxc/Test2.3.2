ff883c68f09b9b3417eea7bcf9d990e54cfaa6e8
jinhuizxc <1004260403@qq.com>
2017-05-22 13:45:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/test232/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.test232;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 测试Android studio 2.3.2以及配置git环境！
+ * classpath 'com.android.tools.build:gradle:2.3.2'
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
